483cdcfa67fb83bd660194d8826ca10fa1785c30
Jose S <santiagojoed@gmail.com>
2022-07-17 20:04:41 +0800

    Updated gradle

--- app/src/main/java/com/codecodecoffee/compose/timeline/MainActivity.kt
+++ app/src/main/java/com/codecodecoffee/compose/timeline/MainActivity.kt
@@ -67,7 +67,7 @@ class MainActivity : ComponentActivity() {
                             .verticalScroll(rememberScrollState())
                             .fillMaxWidth()
                     ) {
-                        
+
 
 
                         CompositionLocalProvider(
